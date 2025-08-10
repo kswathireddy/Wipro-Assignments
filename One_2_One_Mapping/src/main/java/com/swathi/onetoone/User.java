cccae1e1e2517be1f8f23a9e310bfd2376e1e562
kswathireddy <kswathireddy194@gmail.com>
2025-08-10 15:46:38 +0530

    done

---
+++ One_2_One_Mapping/src/main/java/com/swathi/onetoone/User.java
@@ -0,0 +1,51 @@
+package com.swathi.onetoone;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="profile_id")
+	private Profile profile;
+
+	public User() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+	
+
+}
